1583cd5f77a46b279861e2256f4c9fde7ce36294
Ravi Teja <darla.raviteja04@gmail.com>
2026-02-02 05:28:30 +0530

    BackEnd Day-5

---
+++ day-5/src/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+const notes = [];
+
+// Post 
+app.post('/notes', (req, res) => {
+    notes.push(req.body);
+    res.status(201).json({
+        message: "Note Created Successfully...!"
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
