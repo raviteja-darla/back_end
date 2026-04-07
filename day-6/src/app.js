e759baa91fff3415198c614cb2213d0144556d91
Ravi Teja <darla.raviteja04@gmail.com>
2026-04-07 23:57:46 +0530

    Updated with status codes

--- day-6/src/app.js
+++ day-6/src/app.js
@@ -12,12 +12,27 @@ const notes = [];
 
 // Creating POST Api method to create new resources by client
 app.post('/notes', (req, res) => {
-    
+    notes.push(req.body);
+    res.status(201).json({ message: "New resources created successfully" })
 })
+
 // Creating GET Api method to send data in response to client
+app.get('/notes', (res) => {
+    res.status(200).json({
+        message: "Fetched data successfully",
+        notes: notes
+    })
+})
 
 // Creating PATCH Api method to make partial modifications by client
+app.patch('/notes/:idx', (req, res) => {
+    notes [req.params.idx].description = req.body.description;
+    res.status(200).json({ message: "Resources successfully updated" })
+})
 
 // Creating DELETE Api method to delete resources by client
-
+app.delete('/notes/:idx', (req, res) => {
+    delete notes [req.params.idx];
+    res.status(204).json({ message: "Requested resources deleted successfully" })
+})
 module.exports = app;
