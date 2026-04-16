dc8190c81d8654ea756bf4af4f050f42e14fd5ca
Ravi Teja <darla.raviteja04@gmail.com>
2026-04-16 09:15:35 +0530

    Folder Structure Created for Insta-clone-project

--- day-15/src/config/dataBase.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectToDB(){
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log("connected to DataBase")
-}
-
-module.exports = connectToDB;
