5f78ae9cadabde98a2574eb7e257a0e165a041fd
Ravi Teja <darla.raviteja04@gmail.com>
2026-04-16 10:44:55 +0530

    Login API implimented

--- day-15/src/controllers/auth.controllers.js
+++ day-15/src/controllers/auth.controllers.js
@@ -41,4 +41,48 @@ async function registerController (req, res) {
 }
 
 
+async function loginController (req, res) {
+    const { username, email, password } = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            { username: username }, { email: email }
+        ]
+    });
+
+    if( !user ){
+        return res.status(409).json({
+            message: "account not found with this details"
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+    const is_password_valid = hash === user.password;
+
+    if( !is_password_valid ){
+        return res.status(401).json({
+            message: "entered invalid password"
+        })
+    };
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+    });
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "user logedin successfully",
+        user: {
+            username: user.username,
+            email: user.email,
+            bio: user.body,
+            profileImage: user.profileImage
+        }
+    })
+}
+
 module.exports = { registerController, loginController };
